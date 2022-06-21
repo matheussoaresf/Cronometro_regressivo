9e929177792ea3457c207150fae13a11254f77f8
Matheus Soares <97004511+matheussoaresf@users.noreply.github.com>
2022-06-21 17:38:51 -0300

    Add files via upload

---
+++ Codigo_assemble/trabalho.X/build/default/production/projeto.o.d
@@ -0,0 +1,3 @@
+build/default/production/projeto.o:  \
+	C:/Users/mathe/OneDrive/trabalho1/trabalho.X/projeto.asm  \
+
---
+++ Codigo_assemble/trabalho.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=trabalho.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/trabalho.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=trabalho.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/trabalho.x.tar
